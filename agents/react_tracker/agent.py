ae471ed421aff7beeffb15d51d9dde8adbd3200c
Windoser <windoser.ai@gmail.com>
2025-09-29 00:00:52 +0530

    use env for model

--- agents/react_tracker/agent.py
+++ agents/react_tracker/agent.py
@@ -1,13 +1,12 @@
 from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
 from .tools import add_expense, get_categories_subcategories
-from ..common import display_tool_interaction
 import time
+import os
+load_dotenv()
 
 
 def build_agent():
-    load_dotenv()
-
     system_prompt = f"""
 You are an expense tracker assistant. 
 Your job is to assist user to manage all things related to their expenses. 
@@ -21,7 +20,7 @@ For Adding Expense:
 """
 
     return create_react_agent(
-        model="gpt-4o-mini",
+        model=os.getenv("OPENAI_MODEL"),
         tools=[add_expense, get_categories_subcategories],
         prompt=system_prompt,
     )
